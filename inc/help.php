1ea8b3759cfe3dbaad8e2bcb708160f21ba54c1b
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-22 14:51:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/up-down-image-slideshow-gallery/trunk@534715 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/help.php
@@ -0,0 +1,5 @@
+<h2>Plugin Management!</h2>
+Click "Go to - Images Management" button to add/update/delete the images. <br />
+Click "Go to - Gallery Setting" button to update gallery setting.
+<h2>How to use!</h2>
+Short code available here <a target="_blank" href='http://www.gopiplus.com/work/2011/04/25/wordpress-plugin-up-down-image-slideshow-script/'>We have three options check this link</a>
---
+++ inc/setting.js
@@ -0,0 +1,88 @@
+/**
+ *     Up down image slideshow gallery
+ *     Copyright (C) 2011  www.gopiplus.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function udisg_submit()
+{
+	if(document.udisg_form.udisg_path.value=="")
+	{
+		alert("Please enter the image path.")
+		document.udisg_form.udisg_path.focus();
+		return false;
+	}
+	else if(document.udisg_form.udisg_link.value=="")
+	{
+		alert("Please enter the target link.")
+		document.udisg_form.udisg_link.focus();
+		return false;
+	}
+	else if(document.udisg_form.udisg_target.value=="")
+	{
+		alert("Please enter the target status.")
+		document.udisg_form.udisg_target.focus();
+		return false;
+	}
+//	else if(document.udisg_form.udisg_title.value=="")
+//	{
+//		alert("Please enter the image title.")
+//		document.udisg_form.udisg_title.focus();
+//		return false;
+//	}
+	else if(document.udisg_form.udisg_order.value=="")
+	{
+		alert("Please enter the display order, only number.")
+		document.udisg_form.udisg_order.focus();
+		return false;
+	}
+	else if(isNaN(document.udisg_form.udisg_order.value))
+	{
+		alert("Please enter the display order, only number.")
+		document.udisg_form.udisg_order.focus();
+		return false;
+	}
+	else if(document.udisg_form.udisg_status.value=="")
+	{
+		alert("Please select the display status.")
+		document.udisg_form.udisg_status.focus();
+		return false;
+	}
+	else if(document.udisg_form.udisg_type.value=="")
+	{
+		alert("Please enter the gallery type.")
+		document.udisg_form.udisg_type.focus();
+		return false;
+	}
+}
+
+function udisg_delete(id)
+{
+	if(confirm("Do you want to delete this record?"))
+	{
+		document.frm_udisg_display.action="options-general.php?page=up-down-image-slideshow-gallery/image-management.php&AC=DEL&DID="+id;
+		document.frm_udisg_display.submit();
+	}
+}	
+
+function udisg_redirect()
+{
+	window.location = "options-general.php?page=up-down-image-slideshow-gallery/image-management.php";
+}
+
+function udisg_help()
+{
+	window.open("http://www.gopiplus.com/work/2011/04/25/wordpress-plugin-up-down-image-slideshow-script/");
+}
