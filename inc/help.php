60d08ce2601c7d0cb100465bfa5cdb4942611178
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-15 11:52:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/up-down-image-slideshow-gallery/trunk@572561 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/help.php
+++
@@ -1,9 +0,0 @@
-<br /><strong>Plugin Management!</strong>
-<ul>
-<li>Click "Go to - Images Management" button to add/update/delete the images.</li>
-<li>Click "Go to - Gallery Setting" button to update gallery setting.</li>
-</ul>
-<strong>How to use!</strong>
-<ul>
-<li>Check official website for <a target="_blank" href='http://www.gopiplus.com/work/2011/04/25/wordpress-plugin-up-down-image-slideshow-script/'>more info</a></li>
-</ul>
